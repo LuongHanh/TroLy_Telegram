3e54fb01d21909866b4187a0f4edca4b816d44f0
LuongHanh <163307432+LuongHanh@users.noreply.github.com>
2025-08-18 09:55:43 +0700

    fix api admin login

--- web/src/api.js
+++ web/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // backend Express server
+  baseURL: import.meta.env.VITE_API_URL + "/api", // backend Express server
 });
 
 export default api;
